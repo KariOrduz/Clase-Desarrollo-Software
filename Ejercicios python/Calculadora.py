85c33eb83b17e7489ee0acd4d927bc22bc0537e3
Karina <koauis@gmail.com>
2021-09-13 09:06:45 -0500

    Ejemplos python 1

---
+++ Ejercicios python/Calculadora.py
@@ -0,0 +1,17 @@
+class Calculadora:
+    def sumar(self, x, y):
+        return x+y
+
+    def restar(self, x, y):
+        return x-y
+
+   
+
+
+
+
+calc = Calculadora()
+
+print (calc.sumar(3,4))
+
+
